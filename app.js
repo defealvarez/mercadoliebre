2fc45ff42488bafc23c37b84a26a62b44d0ce442
defealvarez <fedealvarez_86@hotmail.com>
2022-01-27 02:00:07 -0300

    commit inicial

---
+++ app.js
@@ -0,0 +1,21 @@
+let express = require('express');
+let app = express();
+let path = require('path');
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Servidor corriendo")
+});
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'));
+});
+
+app.get('/register', (req,res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+});
+
+app.get('/login', (req,res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+});
